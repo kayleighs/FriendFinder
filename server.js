466866bcaa607e7b48c6d14bd493e487d43bb9f0
Kayleighs <47003302+kayleighs@users.noreply.github.com>
2019-07-22 12:13:40 -0400

    server.js, and routes updated, possibly working

--- server.js
+++ server.js
@@ -8,3 +8,18 @@ var PORT = 3000;
 // Sets up the Express app to handle data parsing
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
+
+// Serve static content for the app from the "public" directory in the application directory.
+app.use(express.static("./app/public"));
+
+//ROUTER
+// The below points our server to a series of "route" files.
+// These routes give our server a "map" of how to respond when users visit or request data from various URLs.
+require('./app/routing/api-routes.js')(app);
+require('./app/routing/html-routes.js')(app);
+
+
+//Starts server
+app.listen(PORT, function () {
+    console.log("Server listening on: http://localhost:" + PORT);
+});
